9c74de6cdf66f5f0bd5173c40f437706105186e1
Ian Bobinac <ian@ianb.dev>
2025-03-04 09:00:47 +0100

    feat: add (dev) runtime validation to slotnames

--- src/Slot.tsx
+++ src/Slot.tsx
@@ -1,23 +1,34 @@
 import type { ComponentChildren, VNode } from "preact";
 
 /** Multiple children. For one child, use the regular {children} */
-export type Slottable = VNode<{ slot?: string }>[];
+export type Slottable<T = string> = VNode<{ slot?: T } & { slot: any }>[];
 
 interface Slot {
   name: string;
-  children?: ComponentChildren;
-  from: Slottable;
+  children?: ComponentChildren; // fallback
+  from: Slottable; // all slots
+  usedIn?: (props: any) => any;
 }
 
-export const Slot = ({ name, children: fallback, from }: Slot) => {
-  return from.find((el) => el.props?.slot === name) ?? fallback;
+// TODO: this is pretty bad and hacky
+const findWrongSlots = (usedIn: (props: any) => any, from: Slottable) => {
+  const slotNames = (usedIn.toString().match(/name: "([^"]+)"/g) ?? [])
+    .map((el) => el.replace('name: "', ""))
+    .map((el) => el.replace('"', ""));
+
+  const providedSlotNames = from.map((el) => el.props?.slot);
+  providedSlotNames.map((n) => {
+    if (!slotNames.includes(n)) {
+      console.warn(
+        `Slot '${n}' was passed to ${usedIn.name}, which does not declare it.`
+      );
+    }
+  });
 };
 
-declare module "preact" {
-  namespace JSX {
-    interface IntrinsicAttributes {
-      key?: any;
-      slot?: string;
-    }
+export const Slot = ({ name, children: fallback, from, usedIn }: Slot) => {
+  if (process.env.NODE_ENV === "development") {
+    usedIn && findWrongSlots(usedIn, from);
   }
-}
+  return from.find((el) => el.props?.slot === name) ?? fallback;
+};
