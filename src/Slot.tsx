795dc9d245f8794a1b25d9825bb7ce6a8cdafef1
Ian Bobinac <ian@ianb.dev>
2025-03-10 21:55:55 +0100

    feat(debug): add undefined check to validateSlots

--- src/Slot.tsx
+++ src/Slot.tsx
@@ -17,6 +17,10 @@ const namedlog = (message: string) => {
   console.error("%c [named-slots]", "color: #56b9c8", message);
 };
 
+/**
+ * Check if slots are valid. Detects undefined, duplicates and invalid data-slot names.
+ * @returns {void}
+ */
 export const validateSlots = <T extends unknown = string>(
   children: Slottable<T>,
   slotNames: T[],
@@ -26,27 +30,31 @@ export const validateSlots = <T extends unknown = string>(
 
   const usedSlots: T[] = [];
 
-  const definedIn = inComponent ? `, defined in ${inComponent.name},` : "";
+  const definedIn = inComponent ? `, defined in '${inComponent.name}',` : "";
+
+  const throwOrLog = (message: string) => {
+    if (throws) {
+      throw new Error(message);
+    } else {
+      namedlog(message);
+    }
+  };
 
   children.forEach((child) => {
     const slotName = child.props?.["data-slot"];
+    if (!slotName) {
+      throwOrLog(
+        `A slottable component${definedIn} has been passed children without a data-slot attribute.
+Valid data-slot names are: ${slotNames.join(", ")}`
+      );
+    }
     if (!slotNames.includes(slotName)) {
-      const message = `Slot '${slotName}'${definedIn} is not valid.
-Valid slots are: ${slotNames.join(", ")}`;
-      if (throws) {
-        throw new Error(message);
-      } else {
-        namedlog(message);
-      }
+      throwOrLog(`Slot '${slotName}'${definedIn} is not valid.
+Valid data-slot names are: ${slotNames.join(", ")}`);
     }
     if (usedSlots.includes(slotName)) {
-      const message = `Slot '${slotName}'${definedIn} has been used more than once.
-Each named-slot can be defined and used only once.`;
-      if (throws) {
-        throw new Error(message);
-      } else {
-        namedlog(message);
-      }
+      throwOrLog(`Slot '${slotName}'${definedIn} has been used more than once.
+Each named-slot can be defined and used only once.`);
     }
     usedSlots.push(child.props?.["data-slot"]);
   });
