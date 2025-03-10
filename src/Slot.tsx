f5f6167e1b8e4b4cb2d7220c14b7814ef137c98f
Ian Bobinac <ian@ianb.dev>
2025-03-10 21:34:28 +0100

    feat(debug): add validateSlots export, best effort at runtime "type safety" for slot names

--- src/Slot.tsx
+++ src/Slot.tsx
@@ -1,7 +1,7 @@
 import type { ComponentChildren, VNode } from "preact";
 
 /** Multiple children. For one child, use the regular {children} */
-export type Slottable = VNode<{ slot?: string }>[];
+export type Slottable<T = string> = VNode<{ slot: T }>[];
 
 interface Slot {
   name: string;
@@ -11,6 +11,45 @@ interface Slot {
 
 export const Slot = ({ name, children: fallback, from }: Slot) => {
   return from.find((el) => el.props?.slot === name) ?? fallback;
+};
+
+const namedlog = (message: string) => {
+  console.error("%c [named-slots]", "color: #56b9c8", message);
+};
+
+export const validateSlots = <T extends unknown = string>(
+  children: Slottable<T>,
+  slotNames: T[],
+  { throws, inComponent }: { throws?: boolean; inComponent: Function }
+) => {
+  if (process.env.NODE_ENV !== "development") return;
+
+  const usedSlots: T[] = [];
+
+  const definedIn = inComponent ? `, defined in ${inComponent.name},` : "";
+
+  children.forEach((child) => {
+    const slotName = child.props.slot;
+    if (!slotNames.includes(slotName)) {
+      const message = `Slot '${slotName}'${definedIn} is not valid.
+Valid slots are: ${slotNames.join(", ")}`;
+      if (throws) {
+        throw new Error(message);
+      } else {
+        namedlog(message);
+      }
+    }
+    if (usedSlots.includes(slotName)) {
+      const message = `Slot '${slotName}'${definedIn} has been used more than once.
+Each named-slot can be defined and used only once.`;
+      if (throws) {
+        throw new Error(message);
+      } else {
+        namedlog(message);
+      }
+    }
+    usedSlots.push(child.props.slot);
+  });
 };
 
 declare module "preact" {
