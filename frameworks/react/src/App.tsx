9992514fc91e7d2531a21e0261e8f67c1d32f995
Ian Bobinac <ian@ianb.dev>
2025-03-15 12:33:29 +0100

    feat: add implementation in react

--- frameworks/react/src/App.tsx
+++ frameworks/react/src/App.tsx
@@ -1,5 +1,16 @@
+import { Card } from "./Card";
+import { RandomComponent } from "./RandomComponent";
+
 function App() {
-  return <div></div>;
+  return (
+    <Card>
+      <div data-slot="header" className={"class-from-outside"}>
+        This div is not semantic
+      </div>
+      <RandomComponent data-slot="content" />
+      <footer data-slot="footer">I'm a footer</footer>
+    </Card>
+  );
 }
 
 export default App;
---
+++ frameworks/react/src/Card.tsx
@@ -0,0 +1,21 @@
+import { Slot, Slottable, validateSlots } from "../../../src/Slot";
+
+export const Card = ({ children }: { children: Slottable }) => {
+  validateSlots(children, ["header", "content", "footer"], {
+    inComponent: Card,
+  });
+  return (
+    <div className={"card"}>
+      <Slot name="header" from={children}></Slot>
+      {/* no fallback, renders only if slot is provided */}
+      <Slot name="content" from={children}>
+        <div>Fallback content</div>
+      </Slot>
+      <div className={"class-from-inside"}>
+        <Slot name="footer" from={children}>
+          <div style={{ background: "pink" }}>Fallback footer</div>
+        </Slot>
+      </div>
+    </div>
+  );
+};
